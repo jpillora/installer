6a6362caedb37fa9668a5a3f266823d8388b04b6
Jaime Pillora <dev@jpillora.com>
2015-04-01 21:27:08 +1100

    added install script

--- jpinstaller.go
+++ jpinstaller.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 
 	"github.com/labstack/echo"
 )
+
+var installsh string
 
 func main() {
 	port := os.Getenv("PORT")
@@ -13,12 +17,23 @@ func main() {
 		port = "3000"
 	}
 
+	installsh = load("install")
+
 	e := echo.New()
-	e.Get("/install", install)
+	e.Get("/install/:repo", install)
 	log.Printf("Listening on %s...", port)
 	e.Run(":" + port)
 }
 
 func install(c *echo.Context) {
-	c.String(200, "installingggg")
+	repo := c.P(0)
+	c.String(200, fmt.Sprintf(installsh, repo))
+}
+
+func load(script string) string {
+	b, err := ioutil.ReadFile("scripts/" + script + ".sh")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(b)
 }
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,43 @@
-install.sh
+#!/bin/bash
+
+EXEC="%s"
+# EXEC="chisel"
+
+if ! which curl > /dev/null; then
+	echo "curl is not installed"
+	exit 1
+fi
+
+#find OS
+case `uname -s` in
+Darwin) OS="darwin";;
+Linux) OS="linux";;
+*) echo "unknown os" && exit 1;;
+esac
+#find ARCH
+if uname -m | grep 64 > /dev/null; then
+	ARCH=amd64
+else
+	ARCH=386
+fi
+
+GH="https://github.com/jpillora/$EXEC"
+VERSION=`curl -sI $GH/releases/latest | grep Location | grep -m 1 -o '\d\+\.\d\+\.\d\+'`
+DIR="${EXEC}_${VERSION}_${OS}_${ARCH}"
+echo "Downloading: $DIR"
+URL="$GH/releases/download/$VERSION/$DIR"
+case "$OS" in
+darwin)
+	curl -# -L "$URL.zip" > tmp.zip 
+	unzip -qq tmp.zip
+	rm tmp.zip
+	;;
+linux)
+	curl -# -L "$URL.tar.gz" | tar zxvf -
+	;;
+esac
+
+cp $DIR/$EXEC $EXEC
+chmod +x $EXEC
+rm -r $DIR
+echo "Done"
