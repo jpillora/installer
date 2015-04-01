2624f0b6fc40e99e3cfe8fc83eabadce850e004e
Jaime Pillora <dev@jpillora.com>
2015-04-01 17:21:27 +1100

    init

---
+++ jp-installer.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Printf("Listening on %s...", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
