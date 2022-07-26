83672f4bca4595df0d015bb7125359f7347df2e9
Jaime Pillora <jaime.pillora@x15.com.au>
2022-07-26 21:43:45 +1000

    explicit listen on all interfaces

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
 	"strconv"
 
@@ -14,9 +15,15 @@ var version = "0.0.0-src"
 func main() {
 	c := handler.DefaultConfig
 	opts.New(&c).Repo("github.com/jpillora/installer").Version(version).Parse()
-	log.Printf("Default user is '%s', GH token set: %v, listening on %d...", c.User, c.Token != "", c.Port)
-	h := &handler.Handler{Config: c}
-	if err := http.ListenAndServe(":"+strconv.Itoa(c.Port), h); err != nil {
+	log.Printf("default user is '%s', github token set: %v, listening on %d...", c.User, c.Token != "", c.Port)
+	l, err := net.Listen("tcp4", "0.0.0.0:"+strconv.Itoa(c.Port))
+	if err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("listening on port %d...", c.Port)
+	h := &handler.Handler{Config: c}
+	if err := http.Serve(l, h); err != nil {
+		log.Fatal(err)
+	}
+	log.Print("exiting")
 }
